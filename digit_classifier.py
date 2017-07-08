932a2d5e139e2eb570dd9de7b1515eb02179dd24
m3ller <melcanwong@hotmail.com>
2017-07-08 15:12:24 -0700

    Moved code to view mnist data into its own function. Added a main()

--- digit_classifier.py
+++ digit_classifier.py
@@ -6,13 +6,35 @@ from PIL import Image
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-data, label = mnist.train.next_batch(1)
+def view(data, label, save_image=False):
+    """ View data as a 28-by-28 pixel image, with an option to save the image
+    as 'mnist_<label value>'.png.
 
-w, h = 28, 28
-data = np.ceil(data).astype('uint8') * 255
-data = data.reshape(w, h)
-img = Image.fromarray(data)
-img.save('myNum.png')
-img.show()
+    Reshape numpy array 'data' to a 28-by-28 array and view image.
+    Args:
+        data: 1-by-784 Numpy array. Element values [0,1]
+        label: 1-by-10 Numpy array. One-hot.
+        save_image: Boolean. Whether to save a png of data.
+    """
+    W, H = 28, 28
+    img = np.ceil(data).astype('uint8') * 255
+    img = img.reshape(W, H)
+    img = Image.fromarray(img, 'L')
+
+    img_label = np.where(label)[1][0]
+    if save_image:
+        img.save('./mnist_{0}.png'.format(img_label))
+        
+    img.show()
+    return
+
+def main():
+    mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+    data, label = mnist.train.next_batch(1)
+
+    view(data, label, True)
+
+main()
+
+    
 
